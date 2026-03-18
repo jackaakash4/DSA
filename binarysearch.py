1797cb66707b3f4e3040ede51ce7ffc46eeb919d
Jack Aakash <jackaakash4@gmail.com>
2026-03-18 23:22:44 +0545

    Implementation of binary search in python

---
+++ binarysearch.py
@@ -0,0 +1,25 @@
+#implementation of binary search
+
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+
+        if guess == item:
+            return mid
+
+        elif guess > item:
+            high = mid - 1
+        
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9, 11]
+
+print("The index of 3 is ",binary_search(my_list, 3))
+
+
