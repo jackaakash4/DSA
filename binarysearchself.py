df964a875677421a7ef31c14894033de2d7a36ac
Jack Aakash <jackaakash4@gmail.com>
2026-03-22 22:52:01 +0545

    Self practices binary search

--- binarysearchself.py
+++ binarysearchself.py
@@ -2,21 +2,18 @@
 #self practice binary search
 
 def binarySearch(arr, element):
-    low = arr[0]
-    high = arr[len(arr) - 1]
-    guess = element
+    high = len(arr) - 1
+    low = 0
 
     while low <= high:
         mid = (low + high) // 2
-
-        if guess == arr[mid]:
+        if arr[mid] == element:
             return mid
-        elif guess < arr[mid]:
-            high = mid - 1
-        else:
+        elif arr[mid] < element:
             low = mid + 1
+        else:
+            high = mid -1
     return None
-
 
 print("The position of element is ", binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
 
