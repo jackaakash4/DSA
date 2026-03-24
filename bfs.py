7a17ac3ef698518636011b736308d1e336944386
Jack Aakash <jackaakash4@gmail.com>
2026-03-24 23:49:37 +0545

    BFS implemented in general problem

---
+++ bfs.py
@@ -0,0 +1,21 @@
+#implementation of breadth first search
+from collections import deque
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = set()
+
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.add(person)
+        return False
+
+search("you")
